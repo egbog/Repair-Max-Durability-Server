217e6c4ebf28ada60297daebc905abe7d78957c3
egbog <3422424+egbog@users.noreply.github.com>
2026-07-02 18:31:08 -0700

    version iterate

--- Server/RepairMaxDurabilityServer.cs
+++ Server/RepairMaxDurabilityServer.cs
@@ -16,7 +16,7 @@ public record ModMetadata : AbstractModMetadata {
     public override string                                          Name              { get; init; } = "RepairMaxDurability";
     public override string                                          Author            { get; init; } = "egbog";
     public override List<string>?                                   Contributors      { get; init; }
-    public override SemanticVersioning.Version                      Version           { get; init; } = new("2.0.3");
+    public override SemanticVersioning.Version                      Version           { get; init; } = new("2.1.0");
     public override SemanticVersioning.Range                        SptVersion        { get; init; } = new("~4.0.0");
     public override List<string>?                                   Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>?   ModDependencies   { get; init; }
