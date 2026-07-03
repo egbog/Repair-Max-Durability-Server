3215facbf9a9a3691bb11d3b59ccce608d395d61
egbog <3422424+egbog@users.noreply.github.com>
2026-07-02 18:09:50 -0700

    Delete release-package.bat

--- release-package.bat
+++
@@ -1,5 +0,0 @@
-SET Dir="C:\Users\Ryan\Documents\GitHub\SPT GIT\server\project\user\mods\Repair-Max-Durability-Server\dist"
-
-CD /D C:\Program Files\7-Zip
-
-7z a %Dir%\egbog-RepairMaxDurability.zip %Dir%\BepInEx %Dir%\user
